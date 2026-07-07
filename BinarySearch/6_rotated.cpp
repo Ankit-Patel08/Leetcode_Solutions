0577324c2eda99082fb3c74a0b2b2c47211d9b50
Ankit-Patel08 <ap9838906582@gmail.com>
2026-07-07 13:37:20 +0530

    learned the appying BS on the rotated array

---
+++ BinarySearch/6_rotated.cpp
@@ -0,0 +1,32 @@
+/*
+link ->   https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+*/
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+
+        while(l <= r){
+            int mid = l+(r-l)/2;
+
+            if(target == nums[mid]) return mid;
+            else if(nums[l] <= nums[mid]){
+                if(target >= nums[l] && target <= nums[mid]){
+                    r = mid-1;
+                }else{
+                    l = mid+1;
+                }
+            }else{
+                if(nums[mid] <= target && target <= nums[r]){
+                    l = mid+1;
+                }else{
+                    r = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
---
+++ BinarySearch/7_rotated2.cpp
@@ -0,0 +1,34 @@
+/*
+link ->   https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/\
+
+*/
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        while(l <= r){
+            int mid = l + (r-l);
+            if(nums[mid] == target) return true;
+            else if(nums[l] == nums[mid] && nums[mid] == nums[r]){  // this is extra than the previous rotated because of the duplication element
+                l++;
+                r--;
+            }
+            else if(nums[l] <= nums[mid]){
+                if(nums[l] <= target && target <= nums[mid]){
+                    r = mid-1;
+                }else{
+                    l = mid+1;
+                }
+            }else{
+                if(nums[mid] <= target && target <= nums[r]){
+                    l = mid+1;
+                }else{
+                    r = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
