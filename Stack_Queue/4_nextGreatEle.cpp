6eddd130dcc4fbcc02d8a2cd2283ff0f5acfbd57
Ankit-Patel08 <ap9838906582@gmail.com>
2026-07-15 10:58:33 +0530

    added the code when there is circular allowed

--- Stack_Queue/4_nextGreatEle.cpp
+++ Stack_Queue/4_nextGreatEle.cpp
@@ -31,3 +31,31 @@ public:
         return ans;
     }
 };
+
+
+
+
+// Now when there is circular array then we can just traverse the array twice and use the same logic as above.
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        stack<int>st;
+        vector<int>ans(n);
+        for(int i = 2*n-1; i >= 0; i--){  // we are doing this extra N traversal so that we can store 
+            while(!st.empty() &&  st.top() <= nums[i%n]){ // the max till that in stack bcz of circula
+                st.pop();
+            }
+            if(i < n){
+                if(st.empty()){
+                    ans[i] = -1;
+                }else{
+                    ans[i] = st.top();
+                }
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+};
