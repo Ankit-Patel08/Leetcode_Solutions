1528c2b1bab137507d41729508281706b9ef68cf
Ankit-Patel08 <ap9838906582@gmail.com>
2026-08-26 07:58:30 +0530

    more space optimised

--- DP/3_House_Robber.cpp
+++ DP/3_House_Robber.cpp
@@ -4,6 +4,7 @@ link ->  https://leetcode.com/problems/house-robber/
 
 
 // RECURSION + MEMIOZATION
+// dp -> storing the max robbery possible till that index
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -56,5 +57,28 @@ public:
             dp[i] = max(taking , notTaking);
         }
         return dp[n-1];
+    }
+};
+
+
+// Space optimised
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int taking = nums[0];
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+
+        int notTaking = max(nums[1], nums[0]);
+
+        for(int i = 2; i<n; i++){
+            int curr = max(nums[i]+taking, notTaking);
+            
+            taking = notTaking;
+            notTaking = curr;
+        }
+        return notTaking;
     }
 };
