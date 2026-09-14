c318a701aca6d60b3451bb19a781d653ae331db8
Ankit-Patel08 <ap9838906582@gmail.com>
2026-09-15 05:15:58 +0530

    Moved to the top down approach of DP

--- DP/2D_DP/1_unique_path.cpp
+++ DP/2D_DP/1_unique_path.cpp
@@ -21,6 +21,7 @@ int Recursive_ways(int row, int col){
 }
 
 
+// TIME COMPLEXITY
 /*
 Now this gives the time complexity of the 2^(m+n)
 
@@ -33,10 +34,39 @@ so the same time complexity can become 4^(m*n)  if we are allowed to move in all
 */
 
 
+
+// OPTIMISED SOLUTION 
+/*
+Here we are getting the very high TC so we have to look for the better solution 
+
+  After observation we can see the many overlapping problem so we can think of to store the calculated vlaue 
+   so what this current ROW and COLS saying " the number of ways to reach at row and col if we start from the (0,0)"
+      so we can use this information to store the value so to reduce the TC 
+      and this concept is knows as the DP 
+*/
+
+int Top_down_ways(int row, int col, vector<vector<int>>&dp){
+    if(row == 0 && col == 0) return 1;
+
+    if(row <0 || col<0) return 0;
+
+    if(dp[row][col] != -1) return dp[row][col];
+
+    return dp[row][col] = Top_down_ways(row-1, col,dp) + Top_down_ways(row, col-1, dp);
+}
+
+
+
+
+
 int main(){
     int rows, cols;
     cin>>rows>>cols;
 
+    vector<vector<int>>dp(rows, vector<int>(cols,-1));
+
     cout<<Recursive_ways(rows-1, cols-1);
+
+    cout<<Top_down_ways(rows-1, cols-1, dp);
 
 }
